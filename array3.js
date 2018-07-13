dcce1e29e8e99fd051277496c8af1f212a092003
Panatagama <abigams23@gmail.com>
2018-07-13 11:13:01 +0700

    fixing

--- array3.js
+++ array3.js
@@ -3,58 +3,57 @@ var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca
 function dataHandling() {
   input[1] += 'Elsharawy';
   console.log(input);
-  // console.log(tambahNama);
-  // console.log(input[3][3]);
   var bln = input[3]
-  var splitBln = bln.split('/')
-  // console.log(splitBln);
-  var kata = splitBln[1];
-  // console.log(kata);
+
+  var splitBln = bln.split('/');
+  
+  var kata = Number(splitBln[1]);
   var bulan = '';
 
   switch (kata){
-    case '01' :
+    case 01 :
     bulan = 'Januari'; 
     break;
-    case '02' :
+    case 02 :
     bulan = 'Februari';
     break;
-    case '03' :
+    case 03 :
     bulan = 'Maret';
     break;
-    case '04' :
+    case 04 :
     bulan = 'April';
     break;
-    case '05' :
+    case 05 :
     bulan = 'Mei';
     break;
-    case '06' :
+    case 06 :
     bulan = 'Juni';
     break;
-    case '07' :
+    case 07 :
     bulan = 'Juli';
     break;
-    case '08' :
+    case 08 :
     bulan = 'Agustus';
     break;
-    case '09' :
+    case 09 :
     bulan = 'September';
     break;
-    case '10' :
+    case 10 :
     bulan = 'Oktober';
     break;
-    case '11' :
+    case 11 :
     bulan = 'November';
     break;
-    case '12' :
+    case 12 :
     bulan = 'Desember';
     break;
   };
   console.log(bulan);
 
-  var ubahTgl = [splitBln[2], splitBln[0], splitBln[1]];
+  var ubahTgl = splitBln.sort(function(value1, value2) { return Number(value2) - Number(value1); })
   console.log(ubahTgl);
 
+  splitBln = bln.split('/');
   console.log(splitBln.join('-'));
 
   var nama = input[1].slice(0, 15);
